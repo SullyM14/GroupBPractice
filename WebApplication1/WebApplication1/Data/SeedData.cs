6f763c70c6e9ae18e5c719e5d4b43f5b32bf2b16
Muhammed Suleman Miah <69685679+SullyM14@users.noreply.github.com>
2026-01-20 09:35:07 +0000

    Added the project

---
+++ WebApplication1/WebApplication1/Data/SeedData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication1.Data
+{
+    public class SeedData
+    {
+
+        //New method to add user roles and dummy user accounts
+        //Service Provider allows access to all services in the application
+        //User Manager to allow me to create and manage users
+        //Role Manager to allow me to create and manage roles
+        public static async Task SeedUserRoles(IServiceProvider serviceProvider, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+
+            string[] arrayOfRoles = {"Admin","Student","Guest" }; //Creating the array for all potential roles
+
+            foreach(var role in arrayOfRoles) // Loop through each role in the array
+            {
+                var roleExists = await roleManager.RoleExistsAsync(role); //Check if that role currently exists
+
+                if(roleExists == false) //If the role does not exists
+                {
+                    var FinalRole = new IdentityRole(role); //Converting the role string to data type identity role
+                    await roleManager.CreateAsync(FinalRole); //Add the final role
+                }
+            }
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
